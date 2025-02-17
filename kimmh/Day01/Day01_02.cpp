811aa5c4051494b984d7b78cba74d7b265645e7c
KMH <sts07167@gamil.com>
2025-02-17 23:36:27 +0900

    Day01_Commit

---
+++ kimmh/Day01/Day01_02.cpp
@@ -0,0 +1,36 @@
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <iostream>
+
+using namespace std;
+
+// 제일 작은 수 제거하기 https://school.programmers.co.kr/learn/courses/30/lessons/12935?language=cpp
+
+/*
+문제 설명
+    정수를 저장한 배열, arr 에서 가장 작은 수를 제거한 배열을 리턴하는 함수, solution을 완성해주세요. 
+    단, 리턴하려는 배열이 빈 배열인 경우엔 배열에 -1을 채워 리턴하세요. 
+    예를들어 arr이 [4,3,2,1]인 경우는 [4,3,2]를 리턴 하고, [10]면 [-1]을 리턴 합니다.
+*/
+    
+/* 
+제한 조건
+    arr은 길이 1 이상인 배열입니다.
+    인덱스 i, j에 대해 i ≠ j이면 arr[i] ≠ arr[j] 입니다.
+*/
+
+vector<int> solution(vector<int> arr) {
+    vector<int> answer;
+
+    if (arr.size() == 1) {
+        answer.push_back(-1);
+        return answer;
+    }
+    else {
+        int minIndex = min_element(arr.begin(), arr.end()) - arr.begin();
+        arr.erase(arr.begin() + minIndex);
+        return arr;
+    }
+    return answer;
+}
