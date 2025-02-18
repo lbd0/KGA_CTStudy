0fdfbffcff2587040f8ab153101fb7342453363a
cogusp <84373005+cogusp@users.noreply.github.com>
2025-02-18 22:25:55 +0900

    Modified the folder name

---
+++ leech/20250217/04.cpp
@@ -0,0 +1,17 @@
+/*
+Q. 문자열 곱하기
+
+문자열 my_string과 정수 k가 주어질 때,
+my_string을 k번 반복한 문자열을 return 하는
+solution 함수를 작성해 주세요.
+*/
+
+#include <string>
+
+std::string solution(std::string my_string, int k) {
+    std::string answer = "";
+
+    for (int i = 0; i < k; i++) answer += my_string;
+
+    return answer;
+}
