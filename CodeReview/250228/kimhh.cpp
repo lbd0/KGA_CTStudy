7991f8f3c16cb1c5d6f9d96bcaa5bdffe543c488
kxxhyanghxx <kxxhyanghxx@gmail.com>
2025-02-27 16:22:41 +0900

    Update kimhh.cpp

--- CodeReview/250228/kimhh.cpp
+++ CodeReview/250228/kimhh.cpp
@@ -4,7 +4,6 @@ using namespace std;
 
 int solution(vector<int> nums)
 {
-    int answer = 0;
     map <int, int> pocketmon;
     for(int n : nums) { pocketmon[n]++; }
     return min(pocketmon.size(), nums.size()/2);
