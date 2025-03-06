9540eada0771ec027b3475d835b06eb98926b4ee
KMH <sts07167@gamil.com>
2025-03-06 22:54:35 +0900

    Day13

---
+++ kimmh/Day13/Day13_나머지구하기.cpp
@@ -0,0 +1,11 @@
+/*
+	https://school.programmers.co.kr/learn/courses/30/lessons/120810
+*/
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(int num1, int num2) {
+    return num1 % num2;
+}
