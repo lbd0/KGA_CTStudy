860431113f335a3660a89d742669976ae7eb76c4
poonglyang <162484708+poonglyang@users.noreply.github.com>
2025-03-06 18:57:16 +0900

    Update Kimmh.cpp

--- CodeReview/250307/Kimmh.cpp
+++ CodeReview/250307/Kimmh.cpp
@@ -1,21 +1,40 @@
-for(int s : score) {
+#include <string>
+#include <vector>
+#include <map>
+#include <iostream>
+#include <algorithm>
+#include <queue>
+
+using namespace std;
+
+int solution(int k, int m, vector<int> score) {
+    int answer = 0;
+    
+    sort(score.begin(), score.end());
+    queue<int> q;
+
+    for (int s : score) {
         q.push(s);
     }
     
-    for(int i = 0; i < q.size(); i++) {
-        cout << q.front();
+    for (int i = 0; i < score.size() % m; ++i) {
         q.pop();
     }
     
-    cout << score.size() % m << endl;
+    vector<int> boxs;
     
-    for(int i = 0; i < score.size() % m; ++i) {
+    int count = 0;
+    while (!q.empty()) {
+        if(count % m == 0) {
+            boxs.push_back(q.front());
+        }
         q.pop();
+        count++;
     }
     
-    for(int i = 0; i < q.size(); i++) {
-        cout << q.front();
-        q.pop();
+    for(int box : boxs) {
+        answer += box * m;
     }
     
-    
+    return answer;
+}
