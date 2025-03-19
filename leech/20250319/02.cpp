5ab48f48eb187abf3f20c16dfa79e1b065bd137a
cogusp <84373005+cogusp@users.noreply.github.com>
2025-03-19 21:27:22 +0900

    02.cpp stack -> queue

--- leech/20250319/02.cpp
+++ leech/20250319/02.cpp
@@ -1,9 +1,9 @@
 #include <string>
-#include <stack>
+#include <queue>
 
 bool solution(std::string s)
 {
-    std::stack<char> st;
+    std::queue<char> que;
     
     // 문자열의 처음이 ')'이면 false 반환
     if (s[0] == ')') return false;
@@ -11,22 +11,22 @@ bool solution(std::string s)
     // 문자열의 길이만큼 반복
     for (int i = 0; i < s.length(); ++i)
     {
-        // '('이면 스택에 넣기
+        // '('이면 큐에 넣기
         if (s[i] == '(') 
         {
-            st.push(s[i]);
+            que.push(s[i]);
         }
         // ')'이면
         if (s[i] == ')')
         {
-            // 스택이 비어있다면 false 반환
-            if (st.empty()) return false;
-            // 아니라면 스택에서 원소 빼기
-            st.pop();
+            // 큐가 비어있다면 false 반환
+            if (que.empty()) return false;
+            // 아니라면 큐에서 원소 빼기
+            que.pop();
         }
     }
     
-    // 스택이 비어있다면 괄호가 바르게 짝지어졌기에
-    // 스택이 비었는지 판단하는 st.empty()를 반환
-    return st.empty();
+    // 큐가 비어있다면 괄호가 바르게 짝지어졌기에
+    // 큐가 비었는지 판단하는 empty()를 반환
+    return que.empty();
 }
